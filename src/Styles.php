7c97db24f2b04e996481046b975105f6e9d4918d
André Schwarzer <andre@schwarzer.it>
2020-06-02 20:02:02 +0200

    Version 1.0

---
+++ src/Styles.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Schwarzer\PhpCs;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+class Styles
+{
+    public function __invoke(Finder $finder, array $rules = []): Config
+    {
+        $rules = array_merge(require __DIR__ . '/../rules.php', $rules);
+
+        return Config::create()
+            ->setFinder($finder)
+            ->setRules($rules)
+            ->setRiskyAllowed(true)
+            ->setUsingCache(true);
+    }
+
+    public static function getConfig(Finder $finder, array $rules = []): Config
+    {
+        return (new self())($finder, $rules);
+    }
+}
+
